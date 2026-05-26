9d47d113132a39c1498936aa033b5b6ec307bd8c
aaradhiks <139617847+aaradhiks@users.noreply.github.com>
2026-05-26 15:08:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "Python Developer",
+    "Django Developer",
+    "Backend Developer",
+    "Flutter Learner",
+    "Problem Solver",
+    "Software Engineer",
+  ],
+
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+});
